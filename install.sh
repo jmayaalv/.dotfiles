2b7e88a28c6ca5421afbfd5b422c55b54de01051
Juan E. Maya <jmayaalv@gmail.com>
2018-11-23 17:11:52 +0100

    psqkl

--- install.sh
+++ install.sh
@@ -19,6 +19,7 @@ ln -sfv "$DOTFILES_DIR/zsh/custom.zsh" ~/.oh-my-zsh/custom/
 ln -sfv "$DOTFILES_DIR/iterm/com.googlecode.iterm2.plist" ~/Library/Preferences/com.googlecode.iterm2.plist
 ln -sfv "$DOTFILES_DIR/.emacs.d" ~
 
+ln -sfv  ~/.dotfiles/psql/.psqlrc ~/.psqlrc
 
 if [ -d "$DOTFILES_EXTRA_DIR" -a -f "$DOTFILES_EXTRA_DIR/install.sh" ]; then
   . "$DOTFILES_EXTRA_DIR/install.sh"
