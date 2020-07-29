ad2ba27f7456be8de9bff0ac145268aecc06e13c
Juan E. Maya <jmayaalv@gmail.com>
2020-07-29 15:08:53 +0200

    update elpa

---
+++ .emacs.d/elpa/inf-clojure-20200727.1804/inf-clojure-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "inf-clojure" "20200727.1804" "Run an external Clojure process in an Emacs buffer" '((emacs "24.4") (clojure-mode "5.11")) :commit "477fd7c03a2d7d1c83628db54abf34f5337bc3db" :keywords '("processes" "clojure") :url "http://github.com/clojure-emacs/inf-clojure")
