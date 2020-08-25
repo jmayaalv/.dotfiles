f0cc9df0fb5fce4afd82994231b62ab13a3eeb71
Juan E. Maya <jmayaalv@gmail.com>
2020-08-25 15:13:00 +0200

    weird error with authifo

--- .emacs.d/elpa/counsel-20200818.1428/counsel-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /Users/juan.maya/.emacs.d/elpa/counsel-20200818.1428/counsel.el  -*- no-byte-compile: t -*-
-(define-package "counsel" "20200818.1428" "Various completion functions using Ivy" '((emacs "24.5") (swiper "0.13.0")) :commit "dd43ab1217f72948dc5cd669467e33b8b568db44" :keywords '("convenience" "matching" "tools") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
