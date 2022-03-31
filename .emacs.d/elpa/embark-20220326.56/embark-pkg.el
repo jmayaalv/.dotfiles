aff893d9b7633922cfb0aa4b98d90b0242218517
Juan E Maya <jmayaalv@Juans-MacBook-Pro.local>
2022-03-31 10:30:26 +0200

    improvments

---
+++ .emacs.d/elpa/embark-20220326.56/embark-pkg.el
@@ -0,0 +1,12 @@
+(define-package "embark" "20220326.56" "Conveniently act on minibuffer completions"
+  '((emacs "26.1"))
+  :commit "b80d96ce0ab79e73829322e46c6d7493eb2b8c34" :authors
+  '(("Omar Antolín Camarena" . "omar@matem.unam.mx"))
+  :maintainer
+  '("Omar Antolín Camarena" . "omar@matem.unam.mx")
+  :keywords
+  '("convenience")
+  :url "https://github.com/oantolin/embark")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ .emacs.d/elpa/magit-20220326.946/magit-pkg.el
@@ -0,0 +1,18 @@
+(define-package "magit" "20220326.946" "A Git porcelain inside Emacs."
+  '((emacs "25.1")
+    (dash "20210826")
+    (git-commit "20220222")
+    (magit-section "20220325")
+    (transient "20220325")
+    (with-editor "20220318"))
+  :commit "9a3b8517057a7021d282a0faacba653da2756f7e" :authors
+  '(("Marius Vollmer" . "marius.vollmer@gmail.com")
+    ("Jonas Bernoulli" . "jonas@bernoul.li"))
+  :maintainer
+  '("Jonas Bernoulli" . "jonas@bernoul.li")
+  :keywords
+  '("git" "tools" "vc")
+  :url "https://github.com/magit/magit")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
