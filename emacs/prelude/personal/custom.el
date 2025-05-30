e06ddfcff3a7077c5dc22a00dbda9bccdeebba0f
Juan Maya <juan.maya@kane-group.com>
2025-05-30 13:11:37 +0200

    git rebase --continuecleanup + aider

--- emacs/prelude/personal/custom.el
+++ emacs/prelude/personal/custom.el
@@ -4,10 +4,28 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages nil)
+ '(custom-safe-themes
+   '("09b833239444ac3230f591e35e3c28a4d78f1556b107bafe0eb32b5977204d93"
+     default))
+ '(package-selected-packages
+   '(0x0 ag aider all-the-icons anzu beacon browse-kill-ring buffer-move
+         cider clojure-snippets company consult-lsp crux csv-mode
+         diff-hl diminish dimmer discover discover-my-major
+         doom-modeline easy-kill elisp-slime-nav embark-consult epl
+         exec-path-from-shell expand-region flycheck gist git-modes
+         git-timemachine gnu-elpa-keyring-update guru-mode hl-todo
+         html-to-hiccup imenu-anywhere jet js2-mode json-mode
+         key-chord lsp-treemacs lsp-ui marginalia move-text nlinum
+         operate-on-number orderless projectile rainbow-delimiters
+         rainbow-mode restclient smartparens smartrep super-save try
+         undo-tree vertico volatile-highlights web-mode yaml-mode
+         yasnippet zenburn-theme zop-to-char))
  '(package-vc-selected-packages '((aider :url "https://github.com/tninja/aider.el")))
  '(safe-local-variable-values
-   '((cider-repl-set-type . "clojure-cli")
+   '((cider-ns-refresh-after-fn . "user/start")
+     (cider-ns-refresh-before-fn . "user/stop")
+     (projectile-grep-default-files quote ("*.clj" "*.cljs" "*.cljc"))
+     (cider-repl-set-type . "clojure-cli")
      (cider-preferred-build-tool . "clojure-cli"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
